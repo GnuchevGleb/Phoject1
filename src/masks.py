219e8a6f5b467ce1f4de0946119d3eee8f4ce90c
Gleb <glebvolg@mail.ru>
2025-01-08 20:29:35 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,22 @@
+def get_mask_card_number(card_numbers: int) -> str:
+    """
+    Функция маскировки номера банковской карты
+    Пример работы функции:
+    7000792289606361 входной аргумент
+    7000 79** **** 6361 выход функции
+    """
+
+    card_numbers_str = str(card_numbers)
+    return f"{card_numbers_str[0:4]} {card_numbers_str[4:6]}** **** {card_numbers_str[12:]}"
+
+
+def get_mask_account(bank_accounts: int) -> str:
+    """
+    Функция маскировки номера банковского счета
+    Пример работы функции:
+    73654108430135874305 входной аргумент
+    **4305 выход функции
+    """
+
+    bank_accounts_str = str(bank_accounts)
+    return f"**{bank_accounts_str[-4:]}"
