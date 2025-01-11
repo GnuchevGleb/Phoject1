b5a77da6d0cfef654068bea81af80c5c1b39f510
Gleb <glebvolg@mail.ru>
2025-01-11 10:44:08 +0300

    fixed bugs flake8

--- src/widget.py
+++ src/widget.py
@@ -13,18 +13,18 @@ def mask_account_card(card_number_account: str) -> str:
 
     if "Счет" in card_number_account:
 
-        from masks import get_mask_account # type: ignore
+        from masks import get_mask_account  # type: ignore
 
         return f"Счет {(get_mask_account(str(card_number_account[-20:])))}"
     else:
 
-        from masks import get_mask_card_number # type: ignore
+        from masks import get_mask_card_number  # type: ignore
 
         return f"{card_number_account[:-16]}{(get_mask_card_number(str(card_number_account[-16:])))}"
 
 
-#card_number_account = 'Visa Platinum 8990922113665229'
-#print(mask_account_card(card_number_account))
+# card_number_account = 'Visa Platinum 8990922113665229'
+# print(mask_account_card(card_number_account))
 
 
 def get_date(date_time: str) -> str:
@@ -36,5 +36,5 @@ def get_date(date_time: str) -> str:
     return f"{date_time[0:4]}.{date_time[5:7]}.{date_time[8:10]}"
 
 
-#date_time = '2024-03-11T02:26:18.671407'
-#print(get_date(date_time))
+# date_time = '2024-03-11T02:26:18.671407'
+# print(get_date(date_time))
