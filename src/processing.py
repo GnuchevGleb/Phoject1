e56a14247da49d9eee0efe6412eb1ff62bea81fb
Gleb <glebvolg@mail.ru>
2025-02-01 08:56:48 +0300

    added functionality sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state (dictionaries, states = 'EXECUTED' ):
+def filter_by_state (dictionaries: list[dict], states = 'EXECUTED' ) -> list[dict]:
     """
     функция filter_by_state принимает список словарей и опционально значение для ключа
 state (по умолчанию EXECUTED). Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
@@ -27,3 +27,16 @@ state соответствует указанному значению.
 
 
 
+
+def sort_by_date (dictionaries: list[dict], reverse=True) -> list[dict] :
+    """
+    функция sort_by_date принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание). Функция должна возвращать
+    новый список, отсортированный по дате (date)
+
+Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
+[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    """
+
+
+
+    return sorted(dictionaries, key=lambda x: x['date'],reverse=reverse)
