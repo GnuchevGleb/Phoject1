b09466d1360252dd94f7d0e966260dc415faf40c
Gleb <glebvolg@mail.ru>
2025-02-27 00:54:09 +0300

    добавлен файл decorator.py с функцией логирования log

--- src/decorators.py
+++ src/decorators.py
@@ -1,10 +1,11 @@
 import datetime
 import logging
 
-account_card = "Maestro 1596837868705199"
-
 
 def log(filename=None):
+    """
+    декоратор log автоматически логирует начало и конец выполнения функции, а также ее результаты или возникшие ошибки.
+    """
     logging.basicConfig(
         filename=filename,
         level=logging.INFO,
@@ -14,22 +15,37 @@ def log(filename=None):
     print(f"\n куда выводим:  {filename}")
 
     def wrapper(func):
-        def inner(args):
-            start_time = datetime.datetime.now()
-            logging.info(f"Функция  {func.__name__}")
-            logging.info(f"Время начала выполнения функции  {start_time}")
-            result = func(args)
-            end_time = datetime.datetime.now()
-            logging.info(f"Функция {func.__name__} выполнялась {end_time - start_time} секунд")
-            logging.info(f"my_function ok")
-            return result
 
-        return inner
+        try:
+
+            def inner(args):
+
+                start_time = datetime.datetime.now()
+                logging.info(f"Функция  {func.__name__} ")
+                print((f"Функция  {func.__name__}  {args}"))
+                logging.info(f"Время начала выполнения функции  {start_time}")
+                result = func(args)  # запуск выполнения функции
+                print(result)
+                end_time = datetime.datetime.now()
+                logging.info(f"Функция {func.__name__} выполнялась {end_time - start_time} секунд")
+                logging.info("my_function ok")
+
+                return result
+
+            return inner
+        except FileNotFoundError:
+            logging.info(f"Файл '{filename}' не найден!")  # Если файл не найден, выводим сообщение об ошибке
+        except IOError:
+            logging.info(
+                "Произошла ошибка ввода-вывода при чтении файла!"
+            )  # Если возникает ошибка ввода-вывода, выводим сообщение об ошибке
 
     return wrapper
 
 
-@log("mylog.txt")
+card_number_account = "Maestro 1596837868705199"
+
+
 def mask_account_cards(card_number_account: str) -> str:
     """
     Функция, которая умеет обрабатывать информацию как о картах, так и о счетах
@@ -44,14 +60,14 @@ def mask_account_cards(card_number_account: str) -> str:
     """
 
     logging.info(f"Входные данные: {card_number_account}  ")
-    print(f" Входные данные: {card_number_account}")
+    print(f" Входные данные:::: {card_number_account}")
     if "Счет" in card_number_account:
         if card_number_account[-20:].isdigit():
             from src.masks import get_mask_account  # type: ignore
 
             logging.info(f"Результат выполнения функции    Счет {(get_mask_account(str(card_number_account[-20:])))}")
             return str(f"Счет {(get_mask_account(str(card_number_account[-20:])))}")
-        logging.info(f" ошибка ввода")
+        logging.info(" ошибка ввода")
         return str("ошибка ввода")
 
     if "Maestro" or "MasterCard" in card_number_account:
@@ -61,9 +77,9 @@ def mask_account_cards(card_number_account: str) -> str:
             logging.info(
                 f" Результат выполнения функции  {card_number_account[:-16]}{(get_mask_card_number(str(card_number_account[-16:])))}"
             )
-            return str(f"{card_number_account[:-16]}{(get_mask_card_number(str(card_number_account[-16:])))}")
+            return str(f"{card_number_account[:-16]} {(get_mask_card_number(str(card_number_account[-16:])))}")
         logging.info(f"ошибка ввода")
         return str("ошибка ввода")
 
 
-print(mask_account_cards(account_card))
+print(f" результат {mask_account_cards(card_number_account)}")
--- tests/test_decorators.py
+++
@@ -1,8 +0,0 @@
-from src.decorators import mask_account_cards
-import pytest
-
-
-def test_mask_account_cards(account_card):
-
-    for key in account_card:
-        assert mask_account_cards(str(key[0])) == key[1]
